583e403a3ed24c9180777934e795ed320ddb232f
Lubin <tlb641226830@gmail.com>
2019-05-06 14:56:56 -0700

    Change_background

--- Mirror/js/main.js
+++ Mirror/js/main.js
@@ -7,8 +7,8 @@
 // define globals
 var game;
 var player;
-var PLAYER_WALKING_SPEED = 50;
-var WORLD_SIZE = 3000;
+var PLAYER_WALKING_DRUATION = 50;
+var WORLD_SIZE = 2880;
 var GRID_SIZE = 32;
 // var map;
 // var mapLayer;
