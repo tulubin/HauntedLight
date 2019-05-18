fda45135e20c269112fce20eb9d87eb5ebed3eaa
Lubin <tlb641226830@gmail.com>
2019-05-18 14:19:44 -0700

    update_backup

--- Mirror/js/plugins/LightPlugin.js
+++ Mirror/js/plugins/LightPlugin.js
@@ -6,40 +6,47 @@ function LightPlugin(game) {
 LightPlugin.prototype = Object.create(Phaser.Plugin.prototype);
 LightPlugin.prototype.constructor = LightPlugin;
 
+var wallsBitmap;
+var floor;
+var lightAngle = Math.PI/4;
+var numberOfRays = 20;
+var rayLength = 100;
+
 LightPlugin.prototype.addLight = function() {
-	game.stage.backgroundColor = 0x4488cc;
-	shadowTexture = game.add.bitmapData(game.width, game.height);
-	var lightSprite = game.add.image(game.camera.width/2, game.camera.height/2, shadowTexture);
-	lightSprite.anchor.set(0.5);
-	lightSprite.fixedToCamera = true;
-	lightSprite.blendMode = Phaser.blendModes.MULTIPLY;
+	floor = game.add.sprite(0,0,"floor");
+	wallsBitmap = game.make.bitmapData(640,480);
+	wallsBitmap.draw("walls");
+	wallsBitmap.update();
+	game.add.sprite(0,0,wallsBitmap);
+	cursors = game.input.keyboard.createCursorKeys();
+	maskGraphics = this.game.add.graphics(0, 0);
+	floor.mask=maskGraphics
 };
 LightPlugin.prototype.updateLight = function() {
-	this.updateShadowTexture();
-}
-LightPlugin.prototype.updateShadowTexture = function() {
-	// Draw shadow
-	shadowTexture.context.fillStyle = 'rgb(10, 10, 10)';
-	shadowTexture.context.fillRect(0, 0, game.width, game.height);
-
-	// Draw circle of light with a soft edge
-	gradient = shadowTexture.context.createRadialGradient(game.camera.width/2, game.camera.height/2, LIGHT_RADIUS * 0.75, game.camera.width/2, game.camera.height/2, LIGHT_RADIUS);
-	gradient.addColorStop(0, 'rgba(175, 175, 175, 1.0)');
-	gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
-	gradient.fixedToCamera = true;
-
-	// Draw circle of light
-	shadowTexture.context.beginPath();
-	shadowTexture.context.fillStyle = gradient;
-	// shadowTexture.context.fillStyle = 'rgb(255, 255, 255)';
-	shadowTexture.context.arc(game.camera.width/2, game.camera.height/2, LIGHT_RADIUS, 0, Math.PI*2);
-	// shadowTexture.polygon([Phaser.Point(game.camera.width/2, game.camera.height/2), Phaser.Point(game.camera.width/2, game.camera.height/2), Phaser.Point(game.camera.width/2+100, game.camera.height/2+100), Phaser.Point(game.camera.width/2+100, game.camera.height/2-100)]);
-	shadowTexture.fixedToCamera = true;
-	shadowTexture.context.fill();
-
-	// This just tells the engine it should update the texture cache
-	shadowTexture.dirty = true;
-
-	// Torch: https://gamemechanicexplorer.com/#lighting-3
-	// FlashLight: http://www.html5gamedevs.com/topic/12805-flashlight-effect-and-masking/
+	var mouseAngle = Math.atan2(player.y-game.input.y,player.x-game.input.x);
+	maskGraphics.clear();
+	maskGraphics.lineStyle(2, 0xffffff, 1);
+	maskGraphics.beginFill(0xffff00);
+	maskGraphics.moveTo(player.x,player.y);	
+	for(var i = 0; i<numberOfRays; i++){	
+		var rayAngle = mouseAngle-(lightAngle/2)+(lightAngle/numberOfRays)*i
+		var lastX = player.x;
+		var lastY = player.y;
+		for(var j= 1; j<=rayLength;j+=1){
+      		var landingX = Math.round(player.x-(2*j)*Math.cos(rayAngle));
+      		var landingY = Math.round(player.y-(2*j)*Math.sin(rayAngle));
+      		if(wallsBitmap.getPixel32(landingX,landingY)==0){
+				lastX = landingX;
+				lastY = landingY;	
+			}
+			else{
+				maskGraphics.lineTo(lastX,lastY);
+				break;
+			}
+		}
+		maskGraphics.lineTo(lastX,lastY);
+	}
+	maskGraphics.lineTo(player.x,player.y); 
+ 	maskGraphics.endFill();
+	floor.alpha = 0.5+Math.random()*0.5;	
 };
