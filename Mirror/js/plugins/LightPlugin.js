f978006744b144460db1353b063c6f03d6780d21
Lubin <tlb641226830@gmail.com>
2019-05-20 14:47:40 -0700

    Revert "trying_something"
    This reverts commit 7e64dcb47cfe0df92e491911131686eb6dbc2379.

--- Mirror/js/plugins/LightPlugin.js
+++ Mirror/js/plugins/LightPlugin.js
@@ -7,15 +7,10 @@ function LightPlugin(game) {
 LightPlugin.prototype = Object.create(Phaser.Plugin.prototype);
 LightPlugin.prototype.constructor = LightPlugin;
 
-var shadowTexture;
-
 LightPlugin.prototype.addLight = function() {
-	shadowTexture = game.add.bitmapData(game.width, game.height);
-	shadowTexture.anchor.set(0.5);
-	var maskGraphics = game.add.image(game.camera.width/2, game.camera.height/2, shadowTexture);
-	// maskGraphics = this.game.add.graphics(0, 0);
+	maskGraphics = this.game.add.graphics(0, 0);
 	maskGraphics.blendMode = Phaser.blendModes.MULTIPLY;
-	// maskGraphics.alpha = 0.1;
+	maskGraphics.alpha = 0.1;
 	floorLayer.mask = maskGraphics;
 	terrainLayer.mask = maskGraphics;
 	objectLayer.mask = maskGraphics;
@@ -51,31 +46,4 @@ LightPlugin.prototype.updateLight = function() {
 	maskGraphics.worldAlpha = 0;
 	maskGraphics.endFill();
 	floorLayer.alpha = 0.5+Math.random()*0.5;
-	updateShadowTexture();
-};
-LightPlugin.prototype.updateShadowTexture = function() {
-	// Draw shadow
-	shadowTexture.context.fillStyle = 'rgb(10, 10, 10)';
-	shadowTexture.context.fillRect(0, 0, game.width, game.height);
-
-	// Draw circle of light with a soft edge
-	gradient = shadowTexture.context.createRadialGradient(game.camera.width/2, game.camera.height/2, LIGHT_RADIUS * 0.75, game.camera.width/2, game.camera.height/2, LIGHT_RADIUS);
-	gradient.addColorStop(0, 'rgba(175, 175, 175, 1.0)');
-	gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
-	gradient.fixedToCamera = true;
-
-	// Draw circle of light
-	shadowTexture.context.beginPath();
-	shadowTexture.context.fillStyle = gradient;
-	// shadowTexture.context.fillStyle = 'rgb(255, 255, 255)';
-	shadowTexture.context.arc(game.camera.width/2, game.camera.height/2, LIGHT_RADIUS, 0, Math.PI*2);
-	// shadowTexture.polygon([Phaser.Point(game.camera.width/2, game.camera.height/2), Phaser.Point(game.camera.width/2, game.camera.height/2), Phaser.Point(game.camera.width/2+100, game.camera.height/2+100), Phaser.Point(game.camera.width/2+100, game.camera.height/2-100)]);
-	shadowTexture.fixedToCamera = true;
-	shadowTexture.context.fill();
-
-	// This just tells the engine it should update the texture cache
-	shadowTexture.dirty = true;
-
-	// Torch: https://gamemechanicexplorer.com/#lighting-3
-	// FlashLight: http://www.html5gamedevs.com/topic/12805-flashlight-effect-and-masking/
 };
