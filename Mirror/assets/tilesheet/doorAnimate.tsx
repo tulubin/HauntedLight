7964c6f8fc1d2b1c89b56b346d2d70a0049d3ca1
Lubin <tlb641226830@gmail.com>
2019-05-25 13:52:18 -0700

    add_tilemap

---
+++ Mirror/assets/tilesheet/doorAnimate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="doorAnimate" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="doorAnimate.png" trans="ff00ff" width="128" height="64"/>
+</tileset>
---
+++ Mirror/assets/tilesheet/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="objects" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="objects.png" trans="ff00ff" width="160" height="64"/>
+</tileset>
