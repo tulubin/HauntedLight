b1c0b4cac4eddd09a78c06221615f2720c8d7bff
Ang Li <ali52@ucsc.edu>
2019-06-09 07:12:04 -0700

    implement end image

--- HauntedLight/js/states/boost.js
+++ HauntedLight/js/states/boost.js
@@ -48,6 +48,7 @@ Boost.prototype = {
 		game.load.image('SprintKey', 'assets/img/sprintKey.png');
 		game.load.image('Spacebar', 'assets/img/spacebar.png');
 		game.load.image('Enter', 'assets/img/enter.png');
+		game.load.image('Ending', 'assets/img/ending.png');
 		game.load.atlas('Titlekid', 'assets/img/titlekid.png', 'assets/img/titlekid.json');
 		game.load.atlas('Player', 'assets/img/kid.png', 'assets/img/kid.json');
 		game.load.atlas('Player_f', 'assets/img/kid_flash.png', 'assets/img/kid_flash.json');
